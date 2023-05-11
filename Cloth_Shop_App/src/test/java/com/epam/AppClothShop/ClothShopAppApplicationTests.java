853d4395638680ca1b3b65e80b850bf8c610d43e
Nurzhan <nurzhankydyralieva@gmail.com>
2023-05-11 23:43:20 +0600

    first commit

---
+++ Cloth_Shop_App/src/test/java/com/epam/AppClothShop/ClothShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.AppClothShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
